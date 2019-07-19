df1f42649409d1fee8548240f331d064cf2480e4
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-07-19 13:47:34 -0400

    initial commit

---
+++ js/navTemplate.js
@@ -0,0 +1,12 @@
+const navigation = 
+    `<a href="https://www.linkedin.com/in/jcg408/"><img class="linked" src="image/linkedin.png"
+    alt="linkedin"></a>
+<a href="https://www.github.com/Jcg408"><img class="linked" src="image/github.png" alt="github"></a>
+<ul class="nav nav-pills">
+<li role="presentation"><a href="index.html">Home</a></li>
+<li role="presentation"><a href=#">Blog</a></li>
+<li role="presentation"><a href="projects.html">Projects</a></li>
+</ul>`
+
+document.getElementById('portal').innerHTML = navigation;
+
