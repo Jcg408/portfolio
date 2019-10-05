4d6d96e460e04e9bcc0f8d5f469920ac54874102
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-10-05 15:53:03 -0400

    update with jobtracker project

--- js/navTemplate.js
+++ js/navTemplate.js
@@ -4,7 +4,7 @@ const navigation =
 <a href="https://www.github.com/Jcg408"><img class="linked" src="image/github.png" alt="github"></a>
 <ul class="nav nav-pills">
 <li role="presentation"><a href="index.html">Home</a></li>
-<li role="presentation"><a href=#">Blog</a></li>
+<li role="presentation"><a href="http://www.gladstonetec.com">Blog</a></li>
 <li role="presentation"><a href="projects.html">Projects</a></li>
 </ul>`
 
